26c9e1f885cebec9020736d7e727a83bbd982525
Kamal Dodiya <kjdodiya@gmail.com>
2020-08-20 20:27:11 +0200

    Add GPM logger

---
+++ gpm_logger.py
@@ -0,0 +1,23 @@
+from loguru import logger
+from datetime import datetime
+
+
+class GPMLogger:
+    def __init__(self, name, rotation="1 day"):
+        self.name = name
+        self.info_log_file = "{nm}_info_{year}.log".format(
+            nm=self.name, year=datetime.now().strftime("%Y%m%d")
+        )
+        self.debug_log_file = "{nm}_debug_{year}.log".format(
+            nm=self.name, year=datetime.now().strftime("%Y%m%d")
+        )
+        self.format = "{time} {level} {message}"
+        logger.add(
+            self.info_log_file, format=self.format, level="INFO", rotation=rotation
+        )
+        logger.add(
+            self.debug_log_file, format=self.format, level="DEBUG", rotation=rotation
+        )
+
+    def get_logger(self):
+        return logger
