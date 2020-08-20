7332a050d1d6938cb726cade09f185c8985b979b
Kamal Dodiya <kjdodiya@gmail.com>
2020-08-20 20:27:41 +0200

    Add GPM send reports script

---
+++ gpm_send_reports.py
@@ -0,0 +1,22 @@
+from gpm_mailer import GPMReportMailer
+from datetime import datetime
+import argparse
+
+
+def class_mailer(recipients):
+    sender = "trade-reports-mailer@gpm"
+    last_working_day = datetime.now().strftime("%d %b %Y")
+    file_date = datetime.now().strftime("%d-%b-%Y")
+    report_file_name = "Trade-Status-Report-{dt}.csv".format(dt=file_date)
+    rptm = GPMReportMailer(sender)
+    rptm.compose_email(last_working_day, recipients, report_file_name)
+    rptm.send_mail()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Send Report to recipients")
+    parser.add_argument("-rcpts", "--recipients", help="Comma separated list of email")
+
+    args = parser.parse_args()
+    recipients = args.recipients.split(",")
+    class_mailer(recipients)
