0642eecadba6cdd70a50d28dfc9e954cd90f678e
Kamal Dodiya <kjdodiya@gmail.com>
2020-08-22 11:26:47 +0200

    Add Date args

--- report_generation.sh
+++ report_generation.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+export SMTP_HOST="10.146.1.21"
 readonly sourceFile="/home/kamal/venvs/tradescsv/bin/activate"
 REPORT_SRC_PATH="/home/kamal/trades_writer"
 
 source ${sourceFile}
 cd ${REPORT_SRC_PATH}
+
+rf=`date +'Trade-Status-Report-%d-%b-%Y.csv'`
 
 # Generate Report
 python cards2csv.py
@@ -12,16 +15,14 @@ python cards2csv.py
 # Send to So.  This should be removed once we are happy with email sendingscripts
 python gpm_send_reports.py --recipients so@global-precious-metals.com
 
-rf=`date +'Trade-Status-Report-%d-%b-%Y.csv'`
-
+# Backup original report
 cp $rf  $rf".orig"
 
 # Generate file with dummy content for gpmdev DL
 # This should not contain actual data
 dummy_line="h1,h2,h3,h4,h5\nd11,d12,d13,d14,d15\nd21,d22,d23,d24,d25"
 
-
-
+# write dummy content to file
 echo -e $dummy_line > $rf
 
 # Send this dummmy report to the gpmdev DL
