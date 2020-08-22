cad6dd3b2c8919b884b8cb56f02619c685a5dc48
Kamal Dodiya <kjdodiya@gmail.com>
2020-08-22 11:25:05 +0200

    Pase optional date to report sending script

--- gpm_send_reports.py
+++ gpm_send_reports.py
@@ -1,12 +1,12 @@
 from gpm_mailer import GPMReportMailer
-from datetime import datetime
+from datetime import datetime, timedelta
 import argparse
 
 
-def class_mailer(recipients):
+def class_mailer(recipients, fdt):
     sender = "trade-reports-mailer@gpm"
-    last_working_day = datetime.now().strftime("%d %b %Y")
-    file_date = datetime.now().strftime("%d-%b-%Y")
+    file_date = fdt
+    last_working_day = fdt.replace("-", " ")  # ("%d %b %Y")
     report_file_name = "Trade-Status-Report-{dt}.csv".format(dt=file_date)
     rptm = GPMReportMailer(sender)
     rptm.compose_email(last_working_day, recipients, report_file_name)
@@ -14,11 +14,20 @@ def class_mailer(recipients):
 
 
 if __name__ == "__main__":
+    last_working_day = datetime.now().strftime("%d %b %Y")
+    file_date = datetime.now().strftime("%d-%b-%Y")
+
     parser = argparse.ArgumentParser(description="Send Report to recipients")
     parser.add_argument(
         "-rcpts", "--recipients", required=True, help="Comma separated list of email"
     )
+    parser.add_argument(
+        "-dt", "--date", help="Date of the report to send", default=file_date
+    )
 
     args = parser.parse_args()
+
+    file_date = args.date
     recipients = args.recipients.split(",")
-    class_mailer(recipients)
+
+    class_mailer(recipients, file_date)
